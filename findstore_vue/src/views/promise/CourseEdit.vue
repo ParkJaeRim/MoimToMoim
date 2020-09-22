aa0926ab69c5e34caa6c4176685ae803334c2b4e
김민주 <ynjq01910@naver.com>
2020-09-22 15:03:42 +0900

    [S03P23B301-10] courseEdit.vue 경로 설정, 틀 구현

---
+++ findstore_vue/src/views/promise/CourseEdit.vue
@@ -0,0 +1,35 @@
+<template>
+</template>
+
+<script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
+
+<script>
+import axios from "axios";
+import constants from "../../lib/constants";
+
+const SERVER_URL = constants.ServerUrl;
+
+export default {
+    name: "CourseEdit",
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        initMap() {
+
+        },
+        addScript() {
+
+        },
+    },
+
+}
+</script>
