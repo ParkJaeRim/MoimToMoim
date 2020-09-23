c712e77e1118f5058f07a1e182fd24afbc75d9c6
김민주 <ynjq01910@naver.com>
2020-09-23 15:04:19 +0900

    [S03P23B301-10] courseEdit.vue 코스 틀 구현

--- findstore_vue/src/views/promise/CourseEdit.vue
+++ findstore_vue/src/views/promise/CourseEdit.vue
@@ -1,8 +1,24 @@
 <template>
-  <v-img height="200">
-    <div id="map" style="height:200px;"></div>
-  </v-img>
-</template>
+  <v-container style="max-width: auto;">
+    <v-img height="200">
+      <div id="map" style="height:200px;"></div>
+    </v-img>
+    <template>
+      <v-timeline dense clipped>
+        <v-timeline-item v-for="(n, i) in storeInfos" :key="i" :src="n" color="red lighten-2" large>
+          <template v-slot:opposite>
+            <span>{{i+1}}</span>
+          </template>
+          <v-card class="elevation-2">
+            <v-card-title>{{n.name}}</v-card-title>
+            <v-img :src="n.img"></v-img>
+            <v-card-text>{{n.category}}</v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </template>
+  </v-container>
+</template>  
 
 <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
 
@@ -39,13 +55,13 @@ export default {
         level: this.level,
       };
       var map = new kakao.maps.Map(container, options);
-      
+
       var geocoder = new kakao.maps.services.Geocoder();
       var zoomControl = new kakao.maps.ZoomControl();
-      
+
       map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-      var len = this.storeInfos.length
+      var len = this.storeInfos.length;
       console.log(this.storeInfos);
       geocoder.addressSearch(this.area, function (result, status) {
         if (status === kakao.maps.services.Status.OK) {
@@ -55,7 +71,10 @@ export default {
       });
 
       for (let index = 0; index < len; index++) {
-        geocoder.addressSearch(this.storeInfos[index].address, function (result, status) {
+        geocoder.addressSearch(this.storeInfos[index].address, function (
+          result,
+          status
+        ) {
           if (status === kakao.maps.services.Status.OK) {
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
             var marker = new kakao.maps.Marker({
@@ -65,35 +84,36 @@ export default {
           }
         });
       }
-      
     },
 
     addScript() {
       const script = document.createElement("script");
       script.onload = () => kakao.maps.load(this.initMap);
-      script.src = "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=e03da05ed9076293090e181433ecc1c4&libraries=services";
+      script.src =
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=e03da05ed9076293090e181433ecc1c4&libraries=services";
       document.head.appendChild(script);
     },
 
     getStoreInfo() {
       this.storeInfos = [
         {
-          "id": 1,
-          "name": "롸카두들내쉬빌핫치킨",
-          "address": "서울시 강남구 신사동 646-8",
-          "category": "브런치 / 버거 / 샌드위치",
-          "img": "https://mp-seoul-image-production-s3.mangoplate.com/400192/1272653_1570588239901_12322"
+          id: 1,
+          name: "롸카두들내쉬빌핫치킨",
+          address: "서울시 강남구 신사동 646-8",
+          category: "브런치 / 버거 / 샌드위치",
+          img:
+            "https://mp-seoul-image-production-s3.mangoplate.com/400192/1272653_1570588239901_12322",
         },
         {
-          "id": 19,
-          "name": "시라카와",
-          "address": "서울시 강남구 신사동 664-24 1F",
-          "category": "이자카야 / 오뎅 / 꼬치",
-          "img": "https://mp-seoul-image-production-s3.mangoplate.com/513273_1598598343472200.jpg"
+          id: 19,
+          name: "시라카와",
+          address: "서울시 강남구 신사동 664-24 1F",
+          category: "이자카야 / 오뎅 / 꼬치",
+          img:
+            "https://mp-seoul-image-production-s3.mangoplate.com/513273_1598598343472200.jpg",
         },
-      ]
+      ];
     },
   },
-
 };
 </script>
