2483f4d23114b5337d854d6f0dfd5d26584f6e08
박재림 <dbahck200@gmail.com>
2020-09-25 06:02:48 +0000

    Update constants.js

--- findstore_vue/src/lib/constants.js
+++ findstore_vue/src/lib/constants.js
@@ -3,15 +3,15 @@ export default {
     ServerUrl: "http://j3b301.p.ssafy.io:8000",
     
     // ServerUrl: "http://localhost:8000",
-    // URL_TYPE: {
-    //   MAIN : "mainpage",
-    //   ADMIN : "adminpage",
-    //   USER: {
-    //     SIGNUP: "signup",
-    //     PROFILE: "profile",
-    //     MEMBERPROFILE:"memberprofile",
-    //     FINDPASSWORD: "findPassword",
-    //   },
-    // },
+    URL_TYPE: {
+      MAIN : "mainpage",
+      ADMIN : "adminpage",
+      USER: {
+        SIGNUP: "signup",
+        PROFILE: "profile",
+        MEMBERPROFILE:"memberprofile",
+        FINDPASSWORD: "findPassword",
+      },
+    },
   };
   
