388ba57bdc176792d84656ce00eb0602a83f1d53
박수철 <3864617@naver.com>
2020-10-05 15:01:27 +0900

    mypageerr

--- findstore_vue/src/views/accounts/DetailMain.vue
+++ findstore_vue/src/views/accounts/DetailMain.vue
@@ -58,55 +58,120 @@
           </v-col>
         </v-row>
       </div>
-      <!-- 
-        <div v-if = !promiseList.isfinish>
-          promiseList = promiseListIsfinish0
-        </div>
-        <div v-if = promiseList.isfinish>
-          promiseList = promiseListIsfinish1
-        </div> -->
+    
       <div v-for="item in promiseList" :key="item.id">
-        <br /><br />
-        <v-row>
-          <v-col cols="9" class="pr-0 pb-0">
-            <p class="text-truncate" style="font-size: 15px; text-decoration: underline" v-on:click="goCourse(item.id)">
-              {{ item.title }} / {{ item.date.substring(2, 4) }}.{{
-                item.date.substring(5, 7)
-              }}.{{ item.date.substring(8, 10) }} / {{ item.meeting.title }}
-              <v-badge inline color="deep-purple lighten-4" icon="mdi-lead-pencil"></v-badge>
-            </p>
-          </v-col>
-          <v-col cols="3" class="pt-1 pl-0 yb-0">
-            <!-- p는 패딩 m은 마진 t b l r (top, bottom, left, right) x축 y축 auto 자동 /  -->
-            <v-btn text color="primary" style="font-size: 15px">완료</v-btn>
-          </v-col>
-        </v-row>
-        <v-row>
-          <slider ref="slider" :options="options">
-            <slideritem
-              v-for="(item2, i) in item.reslist"
-              :key="i"
-              :style="styleRecom"
-            >
-              <v-img
-                v-if="item2.img !== null"
-                class="white--text"
-                :src="item2.img"
-                width="120px"
-                height="150px"
-                @click="goStoreDetail(item2.id)"
-              >
-                <div class="transbox white--text">
-                  <div class="store_name">{{ item2.name }}</div>
-                </div>
-              </v-img>
-              <v-img
-                v-if="item2.img == null"
-                class="white--text one"
-                src="http://asq.kr/JNlr0nxp6EQN"
-                width="170px"
-                height="170px"
-                @click="goStoreDetail(item2.id)"
+        <div v-if="item.isfinish == cnt">
+          <br /><br />
+          <v-row>
+            <v-col cols="9" class="pr-0 pb-0">
+              <p class="text-truncate" style="font-size: 15px">
+                {{ item.title }} / {{ item.date.substring(2, 4) }}.{{
+                  item.date.substring(5, 7)
+                }}.{{ item.date.substring(8, 10) }} / {{ item.meeting.title }}
+                <v-badge
+                  inline
+                  color="deep-purple lighten-4"
+                  icon="mdi-lead-pencil"
+                ></v-badge>
+              </p>
+            </v-col>
+
+            <v-col cols="3" class="pt-1 pl-0 yb-0">
+              <!-- p는 패딩 m은 마진 t b l r (top, bottom, left, right) x축 y축 auto 자동 /  -->
+
+              <v-dialog v-model="dialog" max-width="500px">
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    v-if="item.isfinish == 0"
+                    text
+                    color="primary"
+                    style="font-size: 15px"
+                    v-on="on"
+                    >완료</v-btn
+                  >
+                </template>
+
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">리뷰를 남겨주세요.</span>
+                  </v-card-title>
+
+                  <v-container>
+                    <v-card>
+                      <v-row>
+                        <v-col>
+                          <v-card-text>
+                            {{ y }}
+                            {{ item.reslist.length }}
+                            {{ item.reslist[y].name }}
+                            <v-img
+                              v-if="item.reslist[y].img !== null"
+                              class="white--text"
+                              :src="item.reslist[y].img"
+                              width="270px"
+                              height="150px"
+                            ></v-img>
+                            <v-text-field
+                              v-model="reviewdata.rating"
+                              label="여긴 별점을 넣을꺼에요."
+                            >
+                            </v-text-field>
+                            <v-text-field
+                              v-model="reviewdata.review"
+                              label="리뷰를 작성해주세요."
+                              required
+                            >
+                            </v-text-field>
+                          </v-card-text>
+                        </v-col>
+                      </v-row>
+                    </v-card>
+                  </v-container>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      v-if="y > 0"
+                      color="purple lighten-1"
+                      text
+                      @click="y--"
+                    >
+                      이전
+                    </v-btn>
+                    <v-btn
+                      v-if="y < item.reslist.length - 1"
+                      color="purple lighten-1"
+                      text
+                      @click="
+                        pushReviewData(item.reslist[y], item.meeting.id);
+                        y++;
+                      "
+                      >다음</v-btn
+                    >
+                    <v-btn
+                      v-else
+                      color="purple lighten-1"
+                      text
+                      @click="
+                        isfinish(y);
+                        reviewfinish(item.reslist[y], item.meeting.id);
+                        save();
+                      "
+                      >완료</v-btn
+                    >
+                    <v-btn color="purple lighten-1" text @click="close"
+                      >취소</v-btn
+                    >
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-col>
+          </v-row>
+          <v-row>
+            <slider ref="slider" :options="options">
+              <slideritem
+                v-for="(item2, i) in item.reslist"
+                :key="i"
+                :style="styleRecom"
               >
                 <v-img
                   v-if="item2.img !== null"
@@ -114,18 +179,6 @@
                   :src="item2.img"
                   width="120px"
                   height="150px"
-                  @click="goStoreDetail(item2.id)"
-                >
-                  <div class="transbox white--text">
-                    <div class="store_name">{{ item2.name }}</div>
-                  </div>
-                </v-img>
-                <v-img
-                  v-if="item2.img == null"
-                  class="white--text one"
-                  src="http://asq.kr/JNlr0nxp6EQN"
-                  width="170px"
-                  height="170px"
                   @click="goStoreDetail(item2.id)"
                 >
                   <div class="transbox white--text">
@@ -262,7 +315,51 @@ export default {
         name: "courseEdit",
         params: { p_id: id },
       });
-    }
+    },
+    reviewfinish(storeInfos, id) {
+      this.pushReviewData(storeInfos, id);
+      console.log(this.reviews);
+      console.log(storeInfos);
+      console.log(id);
+      axios
+        .post(SERVER_URL + "/api/store/review2/create/", this.reviews)
+        .then(() => {})
+        .catch((err) => console.log(err));
+    },
+    isfinish(k) {
+      const promiseData = this.promiseList[k];
+      // this.pushReviewData(storeInfos,id)
+      // promiseData.isfinish = 1;
+      // console.log(promiseData);
+      axios
+        .post(SERVER_URL + "/promise/update/" + promiseData.id, promiseData)
+        .then(() => {
+          this.promiseData();
+        })
+        .catch((err) => console.log(err));
+    },
+    pushReviewData(storeInfos, id) {
+      this.reviewdata.res_id = storeInfos.id;
+      this.reviewdata.res_name = storeInfos.name;
+      this.reviewdata.user_name = id;
+      // console.log(storeInfos.name);
+      // console.log(id);
+      // console.log(this.reviewdata);
+      // console.log(this.reviews);
+      // console.log(this.reviewdata);
+      this.reviews.push(this.reviewdata);
+      this.reviewdata = Object.assign({}, this.defaultreview);
+
+      // console.log(this.reviewdata);;
+      // console.log(this.reviews);
+    }, // 리뷰 데이터를 reviews에 넣는 작업 과정 과 초기화 과정을 여기서 해주는거다.
+    // res_id 와 res_name , user_id 이거 더 채워주는 작업을 해줘야할듯?? 위에 쓴거중 3-1번과 3-2번 과정임  Success
+    save() {
+      this.dialog = false;
+    },
+    close() {
+      this.dialog = false;
+    },
   },
 };
 </script>
