f71401bee99f9596560dc7a97a75127bd6453bb1
김성웅 <sksk9877@naver.com>
2020-10-07 02:38:00 +0000

    Update constants.js

--- findstore_vue/src/lib/constants.js
+++ findstore_vue/src/lib/constants.js
@@ -1,7 +1,7 @@
 export default {
     TITLE: "Moim으로 모임",
-    // ServerUrl: "http://j3b301.p.ssafy.io:8000",
+    ServerUrl: "http://j3b301.p.ssafy.io:8000",
     
-    ServerUrl: "http://localhost:8000",
+    // ServerUrl: "http://localhost:8000",
   };
   
